952e2bd0c0f935a83e31844e8bf5f58503a6ac2f
Denis M <denis.m.031997@gmail.com>
2023-09-26 18:41:56 +0300

    Implemented mobile options menu, works.

--- proj/js/script.js
+++ proj/js/script.js
@@ -1,23 +1,70 @@
 const headerElsRefAssocArr = {
   navPrimary: document.querySelector(".nav-contacts-primary"),
   navSecondary: document.querySelector(".nav-contacts-secondary"),
+  mobileNav: document.querySelector(".nav-mobile"),
 };
-const headerEl = document.querySelector(".main-header");
-let isMenuRemoved = false; //!
+let isBigMenuRemoved = false; //!
 
+const headerEl = document.querySelector(".main-header");
+
+const optionBtns = {
+  openBtn: headerElsRefAssocArr.mobileNav.querySelector(".open-btn"),
+  closeBtn: headerElsRefAssocArr.mobileNav.querySelector(".close-btn"),
+};
+
+const mobileHeader = document.querySelector(".mobile-header");
+console.log(mobileHeader);
+
+headerElsRefAssocArr.mobileNav.addEventListener("click", toggleMobileOptions);
 window.addEventListener("resize", changeMenuBar);
 
-function changeMenuBar(event) {
-  let bodyWidthPx = document.body.clientWidth;
+initialScreenLoad();
 
-  if (bodyWidthPx <= 1008 && !isMenuRemoved) {
-    isMenuRemoved = true;
+function initialScreenLoad() {
+  const bodyWidthPx = document.body.clientWidth;
+
+  if (bodyWidthPx <= 1008) {
+    isBigMenuRemoved = true;
+    headerEl.removeChild(headerElsRefAssocArr.navPrimary);
+    headerEl.removeChild(headerElsRefAssocArr.navSecondary);
+    headerEl.appendChild(headerElsRefAssocArr.mobileNav);
+
+    headerEl.style.justifyContent = "center";
+  } else {
+    headerEl.appendChild(headerElsRefAssocArr.navPrimary);
+    headerEl.appendChild(headerElsRefAssocArr.navSecondary);
+    headerEl.removeChild(headerElsRefAssocArr.mobileNav);
+
+    headerEl.style.justifyContent = "space-between";
+  }
+}
+
+function changeMenuBar() {
+  const bodyWidthPx = document.body.clientWidth;
+
+  if (bodyWidthPx <= 1008 && !isBigMenuRemoved) {
+    //Detaching elements
+    isBigMenuRemoved = true;
 
     headerEl.removeChild(headerElsRefAssocArr.navPrimary);
     headerEl.removeChild(headerElsRefAssocArr.navSecondary);
-  } else if (bodyWidthPx > 1008 && isMenuRemoved) {
-    isMenuRemoved = false;
+    headerEl.appendChild(headerElsRefAssocArr.mobileNav);
+
+    headerEl.style.justifyContent = "center";
+  } else if (bodyWidthPx > 1008 && isBigMenuRemoved) {
+    //Attaching elements
+    isBigMenuRemoved = false;
+
     headerEl.appendChild(headerElsRefAssocArr.navPrimary);
     headerEl.appendChild(headerElsRefAssocArr.navSecondary);
+    headerEl.removeChild(headerElsRefAssocArr.mobileNav);
+
+    headerEl.style.justifyContent = "space-between";
   }
+}
+
+function toggleMobileOptions() {
+  optionBtns.openBtn.classList.toggle("toggle-display");
+  optionBtns.closeBtn.classList.toggle("toggle-display");
+  mobileHeader.classList.toggle("toggle-display");
 }
